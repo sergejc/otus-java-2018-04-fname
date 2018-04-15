0f24bccbe39c954aed562dab8aa25a1dc379a27c
Sergej Charskij <sergej.charskij@gmail.com>
2018-04-15 16:27:55 +0100

    Added the Google Guava library into the project

---
+++ src/main/java/com/sergejc/Main.java
@@ -0,0 +1,11 @@
+package com.sergejc;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] argv) {
+        List<String> list = Lists.newArrayList("one", "two", "three");
+        System.out.println(list);
+    }
+}
